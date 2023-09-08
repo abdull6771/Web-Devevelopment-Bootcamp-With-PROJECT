ff0e5499d7719a512cc1fba5d49f8133501db60c
abdull6771 <abdullahiahmadm3@gmail.com>
2023-09-08 22:30:07 +0100

    qr-code-project

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,12 @@
+// var generateName = require('sillyname');
+// import generateName from "sillyname";
+// var sillyName = generateName();
+// console.log('My name is '+sillyName)
+import superheroes from "superheroes"
+//const superheroes = require('superheroes');
+ 
+superheroes.all;
+//=> ['3-D Man', 'A-Bomb', …]
+ 
+var hero=superheroes.random();
+console.log("I am "+hero)
